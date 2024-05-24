e89ef09c5df895be9cda8d5ceed95b47f6bd9a2c
Nick Georgi <Nick.Georgi@telekom.de>
2024-05-24 22:26:48 +0200

    frame motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Scroll, ScrollControls } from "@react-three/drei";
+import { Interface } from "./components/Interfaces";
 
 function App() {
   return (
     <>
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={4} damping={0.1}>
+          <Experience />
+          <Scroll html>
+            <Interface/>
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
     </>
   );
 }
