1ca938d259c16f5b78cd4c5f280213e93d375c66
callmewhatuwant <cryptoisgood@outlook.de>
2024-05-27 22:55:11 +0200

    scroll and positioning

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,18 +2,24 @@ import {
   Float,
   MeshDistortMaterial,
   MeshWobbleMaterial,
+  useScroll,
 } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { animate, useMotionValue } from "framer-motion";
 import { motion } from "framer-motion-3d";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { framerMotionConfig } from "../config";
-import { Avatar } from "./Avatar.jsx";
+import { Avatar } from "./Avatar";
+
 import { Office } from "./Office";
 
+
 export const Experience = (props) => {
-  const { section, menuOpened } = props;
+  const { menuOpened } = props;
   const { viewport } = useThree();
+  const data = useScroll();
+
+  const [section, setSection] = useState(0);
 
   const cameraPositionX = useMotionValue();
   const cameraLookAtX = useMotionValue();
@@ -27,13 +33,86 @@ export const Experience = (props) => {
     });
   }, [menuOpened]);
 
+  const characterContainerAboutRef = useRef();
+
+  const [characterAnimation, setCharacterAnimation] = useState("Typing");
+  useEffect(() => {
+    setCharacterAnimation("Falling");
+    setTimeout(() => {
+      setCharacterAnimation(section === 0 ? "Typing" : "Standing");
+    }, 600);
+  }, [section]);
+
   useFrame((state) => {
+    let curSection = Math.floor(data.scroll.current * data.pages);
+
+    if (curSection > 3) {
+      curSection = 3;
+    }
+
+    if (curSection !== section) {
+      setSection(curSection);
+    }
+
     state.camera.position.x = cameraPositionX.get();
     state.camera.lookAt(cameraLookAtX.get(), 0, 0);
+
+    // const position = new THREE.Vector3();
+    // characterContainerAboutRef.current.getWorldPosition(position);
+    // console.log([position.x, position.y, position.z]);
+
+    // const quaternion = new THREE.Quaternion();
+    // characterContainerAboutRef.current.getWorldQuaternion(quaternion);
+    // const euler = new THREE.Euler();
+    // euler.setFromQuaternion(quaternion, "XYZ");
+
+    // console.log([euler.x, euler.y, euler.z]);
   });
 
   return (
     <>
+     
+      <motion.group
+        position={[1.7520128568148856,0.135,2.3750590267873295]}
+        rotation={[-3.141592653589793, 1.1073981633974483, 3.141592653589793]}
+        animate={"" + section}
+        transition={{
+          duration: 0.6,
+        }}
+        variants={{
+          0: {
+            scaleX: 0.9,
+            scaleY: 0.9,
+            scaleZ: 0.9,
+          },
+          1: {
+            y: -viewport.height + 0.5,
+            x: 0,
+            z: 7,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0,
+          },
+          2: {
+            x: -2,
+            y: -viewport.height * 2 + 0.5,
+            z: 0,
+            rotateX: 0,
+            rotateY: Math.PI / 2,
+            rotateZ: 0,
+          },
+          3: {
+            y: -viewport.height * 3 + 1,
+            x: 0.3,
+            z: 8.5,
+            rotateX: 0,
+            rotateY: -Math.PI / 4,
+            rotateZ: 0,
+          },
+        }}
+      >
+        <Avatar animation={characterAnimation} />
+      </motion.group>
       <ambientLight intensity={1} />
       <motion.group
         position={[1.5, 2, 3]}
@@ -44,10 +123,13 @@ export const Experience = (props) => {
         }}
       >
         <Office section={section} />
-
-        <group name="Empty" position={[-0.293, 0.125, -0.689]} rotation={[-Math.PI, 0.322, -Math.PI]} scale={1.05}>
-          <Avatar animation ={section === 0 ? "Typing": "Standing"}/>
-          </group>
+        <group
+          ref={characterContainerAboutRef}
+          name="CharacterSpot"
+          position={[-0.293, 0.125, -0.689]}
+          rotation={[-Math.PI, 0.322, -Math.PI]} 
+          scale={1.05}
+        ></group>
       </motion.group>
 
       {/* SKILLS */}
@@ -95,8 +177,8 @@ export const Experience = (props) => {
             />
           </mesh>
         </Float>
-       
       </motion.group>
+      
     </>
   );
 };
