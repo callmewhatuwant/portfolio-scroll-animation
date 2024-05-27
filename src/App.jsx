9aa0e278329e602d42835e4620f7996ace1eb634
Nick Georgi <Nick.Georgi@telekom.de>
2024-05-27 10:45:39 +0200

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
 import { Scroll, ScrollControls } from "@react-three/drei";
-import { Interface } from "./components/Interfaces";
-import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useEffect, useState } from "react";
+import { Experience } from "./components/Experience";
+import { Menu } from "./components/Menu";
 import { ScrollManager } from "./components/ScrollManager";
+import { Interface } from "./components/Interfaces";
+
 
 function App() {
-  const [section, setsection] = useState(0);
+  const [section, setSection] = useState(0);
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  useEffect(() => {
+    setMenuOpened(false);
+  }, [section]);
 
   return (
     <>
-      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-        <color attach="background" args={["#ececec"]} />
-        <ScrollControls pages={4} damping={0.1}>
-          <ScrollManager secion={section} onSectionChange = {setsection}/>
-          <Experience />
-          <Scroll html>
-            <Interface/>
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-    </>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 42 }}>
+          <color attach="background" args={["#e6e7ff"]} />
+          <ScrollControls pages={4} damping={0.1}>
+            <ScrollManager section={section} onSectionChange={setSection} />
+            <Scroll>
+              <Experience section={section} menuOpened={menuOpened} />
+            </Scroll>
+            <Scroll html>
+              <Interface />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu
+          onSectionChange={setSection}
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+        />
+     </>
   );
 }
 
