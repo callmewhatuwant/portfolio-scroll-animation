21f048c1d7f8a52b4c312f8c333025373ea45641
Nick Georgi <Nick.Georgi@telekom.de>
2024-05-27 10:12:02 +0200

    gsap

--- src/components/ScrollManager.jsx
+++ src/components/ScrollManager.jsx
@@ -1,5 +1,55 @@
+import { useEffect, useRef } from "react";
+import { useScroll } from "@react-three/drei";
+import gsap from "gsap";
+import { useFrame } from "@react-three/fiber";
+
+
+
 export const ScrollManager = (props) =>{
     const {section, onSectionChange} = props;
+
+    const data = useScroll();
+    const lastScroll = useRef(0);
+    const isAnimating = useRef(false);
+
+    useEffect(() => {
+        gsap.to(data.el,{
+            duration: 1,
+            scrollTop: section * data.el.clientHeight,
+            onStart: () => {
+                isAnimating.current = true;
+            },
+            onComplete: () => {
+                isAnimating.current = false;
+            }
+        })
+
+
+
+    }, [section]);
+
+
+
+    useFrame(() => {
+        if (isAnimating.current){
+            lastScroll.current = data.scroll.current;
+            return;
+        }
+    
+
+    const curSection = Math.floor(data.scroll.current * data.pages); 
+    if (data.scroll.current > lastScroll.current && curSection ===0){
+        onSectionChange(1);
+    }
+    if (data.scroll.current < lastScroll.current && 
+        data.scroll.current < 1 / (data.pages -1)
+    ) {
+        onSectionChange(0);
+    }
+
+
+    })
+
+
     return null;
 };
-
